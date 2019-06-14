7a31b0ed2cc25a5ed3417ebbda200421d9510e87
cgad <35946778+cgad@users.noreply.github.com>
2019-06-14 16:41:28 -0600

    Search properties api route, flattened properties schema

--- models/Property.js
+++ models/Property.js
@@ -7,50 +7,43 @@ const PropertySchema = new Schema(
       type: Number,
       required: true
     },
-    location: {
-      neighborhood: {
-        type: String,
-        required: true
-      },
-      address: {
-        houseNumber: {
-          type: Number,
-          required: true
-        },
-        street: {
-          type: String,
-          required: true
-        },
-        city: {
-          type: String,
-          required: true
-        },
-        state: {
-          type: String,
-          required: true
-        },
-        zip: {
-          type: Number,
-          required: true
-        }
-      },
-      subdivision: {
-        type: String
-      },
-      school: {
-        district: {
-          type: String
-        },
-        elementary: {
-          type: String
-        },
-        middle: {
-          type: String
-        },
-        high: {
-          type: String
-        }
-      }
+    neighborhood: {
+      type: String
+    },
+    houseNumber: {
+      type: Number,
+      required: true
+    },
+    street: {
+      type: String,
+      required: true
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    state: {
+      type: String,
+      required: true
+    },
+    zip: {
+      type: Number,
+      required: true
+    },
+    subdivision: {
+      type: String
+    },
+    schoolDistrict: {
+      type: String
+    },
+    elementarySch: {
+      type: String
+    },
+    middleSch: {
+      type: String
+    },
+    highSch: {
+      type: String
     },
     description: {
       type: String,
@@ -68,130 +61,118 @@ const PropertySchema = new Schema(
     openHouse: {
       type: Date
     },
-    interior: {
-      beds: {
-        type: Number
-      },
-      baths: {
-        type: Number
-      },
-      masterBedLoc: {
-        type: String
-      },
-      features: {
-        // breakfast nook, elevator etc
-        type: String
-      },
-      appliances: {
-        type: String
-      },
-      fireplaceFeats: {
-        type: String
-      },
-      fireplaceLoc: {
-        type: String
-      },
-      numFireplaces: {
-        type: Number
-      },
-      basementType: {
-        type: String
-      }
+    beds: {
+      type: Number
     },
-    exterior: {
-      garage: {
-        type: Boolean
-      },
-      garageType: {
-        type: String
-      },
-      outdoorFeats: {
-        type: String
-      },
-      parkingFeats: {
-        type: String
-      },
-      horses: {
-        type: Boolean
-      },
-      horseFacilities: {
-        type: String
-      }
+    baths: {
+      type: Number
     },
-    size: {
-      acres: {
-        type: Number
-      },
-      sqftTotal: {
-        type: Number
-      },
-      sqftBasement: {
-        type: Number
-      },
-      sqftAboveGrd: {
-        type: Number
-      },
-      sqftFinished: {
-        type: Number
-      }
+    masterBedLoc: {
+      type: String
     },
-    property: {
-      type: {
-        // land, attached single family etc
-        type: String
-      },
-      numUnits: {
-        type: Number
-      },
-      floorsInUnit: {
-        type: Number
-      },
-      directionFaces: {
-        type: String
-      },
-      view: {
-        type: String
-      },
-      yearBuilt: {
-        type: Number
-      }
+    interiorFeats: {
+      // breakfast nook, elevator etc
+      type: String
     },
-    laundry: {
-      availability: {
-        type: String
-      },
-      location: {
-        type: String
-      }
+    appliances: {
+      type: String
     },
-    construction: {
-      builderName: {
-        type: String
-      },
-      buildingType: {
-        type: String
-      },
-      materials: {
-        type: String
-      },
-      details: {
-        type: String
-      },
-      architecture: {
-        type: String
-      },
-      flooring: {
-        type: String
-      },
-      houseType: {
-        type: String
-      },
-      cooling: {
-        type: String
-      },
-      otherHVAC: {
-        type: String
-      }
+    fireplaceFeats: {
+      type: String
+    },
+    fireplaceLoc: {
+      type: String
+    },
+    numFireplaces: {
+      type: Number
+    },
+    basementType: {
+      type: String
+    },
+    garage: {
+      type: Boolean
+    },
+    garageType: {
+      type: String
+    },
+    outdoorFeats: {
+      type: String
+    },
+    parkingFeats: {
+      type: String
+    },
+    horses: {
+      type: Boolean
+    },
+    horseFacilities: {
+      type: String
+    },
+    acres: {
+      type: Number
+    },
+    sqftTotal: {
+      type: Number
+    },
+    sqftBasement: {
+      type: Number
+    },
+    sqftAboveGrd: {
+      type: Number
+    },
+    sqftFinished: {
+      type: Number
+    },
+    propType: {
+      // land, attached single family etc
+      type: String
+    },
+    numUnits: {
+      type: Number
+    },
+    floorsInUnit: {
+      type: Number
+    },
+    directionFaces: {
+      type: String
+    },
+    propView: {
+      type: String
+    },
+    yearBuilt: {
+      type: Number
+    },
+    laundryAvailability: {
+      type: String
+    },
+    laundryLocation: {
+      type: String
+    },
+    builderName: {
+      type: String
+    },
+    buildingType: {
+      type: String
+    },
+    constrMaterials: {
+      type: String
+    },
+    constrDetails: {
+      type: String
+    },
+    architecture: {
+      type: String
+    },
+    flooring: {
+      type: String
+    },
+    houseType: {
+      type: String
+    },
+    cooling: {
+      type: String
+    },
+    otherHVAC: {
+      type: String
     },
     buyerTourRegion: {
       type: String
@@ -205,13 +186,11 @@ const PropertySchema = new Schema(
     approvalCond: {
       type: String
     },
-    HOA: {
-      hasHOA: {
-        type: Boolean
-      },
-      HOAfee: {
-        type: Number
-      }
+    hasHOA: {
+      type: Boolean
+    },
+    HOAfee: {
+      type: Number
     },
     taxAmount: {
       type: Number
@@ -219,13 +198,11 @@ const PropertySchema = new Schema(
     restrictions: {
       type: String
     },
-    partialOwnership: {
-      type: {
-        type: String
-      },
-      amount: {
-        type: Number
-      }
+    partialOwnershipType: {
+      type: String
+    },
+    partialOwnershipAmount: {
+      type: Number
     },
     sellerType: {
       type: String
@@ -243,10 +220,7 @@ const PropertySchema = new Schema(
     group: {
       // RE/MAX etc
       type: String
-    },
-    dateAdded: {
-      type: Date,
-      default: Date.now
+      // required: true
     },
     newToSite: {
       type: Boolean
@@ -266,6 +240,270 @@ const PropertySchema = new Schema(
     timestamps: true
   }
 );
+
+// nested objects schema--
+
+// const PropertySchema = new Schema(
+//   {
+//     price: {
+//       type: Number,
+//       required: true
+//     },
+//     location: {
+//       neighborhood: {
+//         type: String
+//       },
+//       address: {
+//         houseNumber: {
+//           type: Number,
+//           required: true
+//         },
+//         street: {
+//           type: String,
+//           required: true
+//         },
+//         city: {
+//           type: String,
+//           required: true
+//         },
+//         state: {
+//           type: String,
+//           required: true
+//         },
+//         zip: {
+//           type: Number,
+//           required: true
+//         }
+//       },
+//       subdivision: {
+//         type: String
+//       },
+//       school: {
+//         district: {
+//           type: String
+//         },
+//         elementary: {
+//           type: String
+//         },
+//         middle: {
+//           type: String
+//         },
+//         high: {
+//           type: String
+//         }
+//       }
+//     },
+//     description: {
+//       type: String,
+//       required: true
+//     },
+//     forSale: {
+//       type: Boolean
+//     },
+//     forRent: {
+//       type: Boolean
+//     },
+//     priceDrop: {
+//       type: Date
+//     },
+//     openHouse: {
+//       type: Date
+//     },
+//     interior: {
+//       beds: {
+//         type: Number
+//       },
+//       baths: {
+//         type: Number
+//       },
+//       masterBedLoc: {
+//         type: String
+//       },
+//       features: {
+//         // breakfast nook, elevator etc
+//         type: String
+//       },
+//       appliances: {
+//         type: String
+//       },
+//       fireplaceFeats: {
+//         type: String
+//       },
+//       fireplaceLoc: {
+//         type: String
+//       },
+//       numFireplaces: {
+//         type: Number
+//       },
+//       basementType: {
+//         type: String
+//       }
+//     },
+//     exterior: {
+//       garage: {
+//         type: Boolean
+//       },
+//       garageType: {
+//         type: String
+//       },
+//       outdoorFeats: {
+//         type: String
+//       },
+//       parkingFeats: {
+//         type: String
+//       },
+//       horses: {
+//         type: Boolean
+//       },
+//       horseFacilities: {
+//         type: String
+//       }
+//     },
+//     size: {
+//       acres: {
+//         type: Number
+//       },
+//       sqftTotal: {
+//         type: Number
+//       },
+//       sqftBasement: {
+//         type: Number
+//       },
+//       sqftAboveGrd: {
+//         type: Number
+//       },
+//       sqftFinished: {
+//         type: Number
+//       }
+//     },
+//     property: {
+//       type: {
+//         // land, attached single family etc
+//         type: String
+//       },
+//       numUnits: {
+//         type: Number
+//       },
+//       floorsInUnit: {
+//         type: Number
+//       },
+//       directionFaces: {
+//         type: String
+//       },
+//       view: {
+//         type: String
+//       },
+//       yearBuilt: {
+//         type: Number
+//       }
+//     },
+//     laundry: {
+//       availability: {
+//         type: String
+//       },
+//       location: {
+//         type: String
+//       }
+//     },
+//     construction: {
+//       builderName: {
+//         type: String
+//       },
+//       buildingType: {
+//         type: String
+//       },
+//       materials: {
+//         type: String
+//       },
+//       details: {
+//         type: String
+//       },
+//       architecture: {
+//         type: String
+//       },
+//       flooring: {
+//         type: String
+//       },
+//       houseType: {
+//         type: String
+//       },
+//       cooling: {
+//         type: String
+//       },
+//       otherHVAC: {
+//         type: String
+//       }
+//     },
+//     buyerTourRegion: {
+//       type: String
+//     },
+//     terms: {
+//       type: String
+//     },
+//     financing: {
+//       type: String
+//     },
+//     approvalCond: {
+//       type: String
+//     },
+//     HOA: {
+//       hasHOA: {
+//         type: Boolean
+//       },
+//       HOAfee: {
+//         type: Number
+//       }
+//     },
+//     taxAmount: {
+//       type: Number
+//     },
+//     restrictions: {
+//       type: String
+//     },
+//     partialOwnership: {
+//       type: {
+//         type: String
+//       },
+//       amount: {
+//         type: Number
+//       }
+//     },
+//     sellerType: {
+//       type: String
+//     },
+//     siteFeatures: {
+//       // adjacent to park, corner lot etc
+//       type: [String]
+//     },
+//     link: {
+//       type: String
+//     },
+//     MLSNumber: {
+//       type: String
+//     },
+//     group: {
+//       // RE/MAX etc
+//       type: String
+//       // required: true
+//     },
+//     newToSite: {
+//       type: Boolean
+//     },
+//     active: {
+//       type: Boolean
+//     },
+//     // photos: {
+//     //   data: Buffer,
+//     //   contentType: String
+//     // }
+//     photos: {
+//       type: [String]
+//     }
+//   },
+//   {
+//     timestamps: true
+//   }
+// );
 
 const Property = mongoose.model("properties", PropertySchema);
 
--- routes/api/properties.js
+++ routes/api/properties.js
@@ -5,38 +5,137 @@ const router = express.Router();
 // load Property model
 const Property = require("../../models/Property");
 
-// @ROUTE /api/properties/test
+// @ROUTE GET /api/properties/test
 // @DESCRIPTION test
 // @ACCESS public
 router.get("/test", (req, res) => {
   res.json({ msg: "success" });
 });
 
-// @ROUTE /api/properties/
-// @DESCRIPTION get all properties
+// @ROUTE GET /api/properties/view/:id
+// @DESCRIPTION get property by id
 // @ACCESS public
-router.get("/", (req, res) => {
-  Property.find()
+router.get("/view/:id", (req, res) => {
+  Property.findById(req.params.id)
+    .then(property => {
+      res.json(property);
+    })
+    .catch(err =>
+      res.status(404).json({ propertynotfound: "Property not found" })
+    );
+});
+
+// @ROUTE GET /api/properties/search
+// @DESCRIPTION get properties according to search filters. if no filters, get all properties
+// @ACCESS public
+router.get("/search", (req, res) => {
+  // build out query object according to user's search filters
+  let query = {};
+
+  for (let key in req.body) {
+    query[key] = req.body[key];
+  }
+
+  // take min and max keys back out cause those aren't present in the schema, only price
+  delete query.min;
+  delete query.max;
+
+  // add price to query
+  if (req.body.min && req.body.max) {
+    query.price = { $gte: req.body.min, $lte: req.body.max };
+  } else if (req.body.min) {
+    query.price = { $gte: req.body.min };
+  } else if (req.body.max) {
+    query.price = { $lte: req.body.max };
+  }
+
+  Property.find(query)
     .then(properties => {
       res.json(properties);
     })
-    .catch(err => res.json("Error: " + err));
+    .catch(err =>
+      res.status(404).json({ nopropsfound: "No properties found" })
+    );
 });
 
+// @ROUTE POST /api/properties/
+// @DESCRIPTION add properties to db - for dev
+// @ACCESS public
 router.post("/", (req, res) => {
+  // flattened
   const newProperty = new Property({
     price: req.body.price,
-    location: {
-      neighborhood: req.body.neighborhood,
-      address: {
-        houseNumber: req.body.houseNumber,
-        street: req.body.street,
-        city: req.body.city,
-        state: req.body.state,
-        zip: req.body.zip
-      }
-    },
-    description: req.body.description
+    neighborhood: req.body.neighborhood,
+    houseNumber: req.body.houseNumber,
+    street: req.body.street,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    subdivision: req.body.subdivision,
+    schoolDistrict: req.body.district,
+    elementarySch: req.body.elementary,
+    middleSch: req.body.middle,
+    highSch: req.body.high,
+    description: req.body.description,
+    forSale: req.body.forSale,
+    forRent: req.body.forRent,
+    priceDrop: req.body.priceDrop,
+    openHouse: req.body.openHouse,
+    beds: req.body.beds,
+    baths: req.body.baths,
+    masterBedLoc: req.body.masterBedLoc,
+    interiorFeatures: req.body.features,
+    appliances: req.body.appliances,
+    fireplaceFeats: req.body.fireplaceFeats,
+    fireplaceLoc: req.body.fireplaceLoc,
+    numFireplaces: req.body.numFireplaces,
+    basementType: req.body.basementType,
+    garage: req.body.garage,
+    garageType: req.body.garageType,
+    outdoorFeats: req.body.outdoorFeats,
+    parkingFeats: req.body.parkingFeats,
+    horses: req.body.horses,
+    horseFacilities: req.body.horseFacilities,
+    acres: req.body.acres,
+    sqftTotal: req.body.sqftTotal,
+    sqftBasement: req.body.sqftBasement,
+    sqftAboveGrd: req.body.sqftAboveGrd,
+    sqftFinished: req.body.sqftFinished,
+    propType: req.body.propType,
+    numUnits: req.body.numUnits,
+    floorsInUnit: req.body.floorsInUnit,
+    directionFaces: req.body.directionFaces,
+    propView: req.body.view,
+    yearBuilt: req.body.yearBuilt,
+    laundryAvailability: req.body.availability,
+    laundryLocation: req.body.location,
+    builderName: req.body.builderName,
+    buildingType: req.body.buildingType,
+    constrMaterials: req.body.materials,
+    constrDetails: req.body.details,
+    architecture: req.body.architecture,
+    flooring: req.body.flooring,
+    houseType: req.body.houseType,
+    cooling: req.body.cooling,
+    otherHVAC: req.body.otherHVAC,
+    buyerTourRegion: req.body.buyerTourRegion,
+    terms: req.body.terms,
+    financing: req.body.financing,
+    approvalCond: req.body.approvalCond,
+    hasHOA: req.body.hasHOA,
+    HOAfee: req.body.HOAfee,
+    taxAmount: req.body.HOAfee,
+    restrictions: req.body.HOAfee,
+    partialOwnershipType: req.body.partialOwnershipType,
+    partialOwnershipAmount: req.body.partialOwnershipAmount,
+    sellerType: req.body.sellerType,
+    siteFeatures: req.body.siteFeatures,
+    link: req.body.link,
+    MLSNumber: req.body.MLSNumber,
+    group: req.body.group,
+    newToSite: req.body.newToSite,
+    active: req.body.active,
+    photos: req.body.photos
   });
 
   newProperty
